47cf2dc0ca626fd1415d72d73ab434175ba6bec1
jibontech <jibon.belaface@gmail.com>
2024-09-22 14:59:15 +0600

    added list

--- home/views.py
+++ home/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from home.models import Slider
+from products.models import Category, Products, Sales
+from datetime import date, datetime
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    now = datetime.now()
+    current_year = now.strftime("%Y")
+    current_month = now.strftime("%m")
+    current_day = now.strftime("%d")
+    categories = len(Category.objects.all())
+    products = len(Products.objects.all())
+    transaction = len(Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ))
+    today_sales = Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ).all()
+    total_sales = sum(today_sales.values_list('grand_total',flat=True))
+    context = {
+        'page_title':'Home',
+        'categories' : categories,
+        'products' : products,
+        'transaction' : transaction,
+        'total_sales' : total_sales,
+    }
+    return render(request, 'home/home.html',context)
+
